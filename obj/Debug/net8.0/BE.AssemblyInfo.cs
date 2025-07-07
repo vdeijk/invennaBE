1ac614c9242ffc48d87b9e213a89790e06ac6f7c
Rick van der Eijk <vandereijkwebdevelopment@gmail.com>
2025-07-07 11:35:21 +0200

    small updates

--- obj/Debug/net8.0/BE.AssemblyInfo.cs
+++ obj/Debug/net8.0/BE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff7c2bd847c37a294aec2a6245ddae437c17e4ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eadcbb3991b57ee7afd8fc401f0fec8e83765b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
